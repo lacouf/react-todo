907d1a0f0643a53d917d028c2af599723ab390e9
Francois Lacoursiere <lacouf@gmail.com>
2017-09-23 13:14:33 -0400

    button onClick = edit mode

--- src/TodosListItem.js
+++ src/TodosListItem.js
@@ -2,16 +2,42 @@ import React, { Component } from 'react';
 import './App.css';
 
 export default class TodosListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: false
+    }
+  }
+
+  renderActionSection() {
+    if (this.state.isEditing) {
+      return (
+        <td>
+          <button>Save</button>
+          <button>Cancel</button>
+        </td>
+      )
+    }
+
+    return (
+      <td>
+        <button onClick={this.onEditClick.bind(this)}>Edit</button>
+        <button>Delete</button>
+      </td>
+    )
+  }
+
   render() {
 
     return (
         <tr>
           <td>{this.props.task}</td>
-          <td>
-            <button>Edit</button>
-            <button>Delete</button>
-          </td>
+          {this.renderActionSection()}
         </tr>
     );
+  }
+
+  onEditClick() {
+    this.setState({ isEditing: true});
   }
 }
