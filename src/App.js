f849ffe3ddb0ddc61e96f99049730c0e4eae9318
Francois Lacoursiere <lacouf@gmail.com>
2017-09-23 11:34:41 -0400

    Pass parameters to TodosList

--- src/App.js
+++ src/App.js
@@ -14,11 +14,18 @@ const todos = [
 ];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>React Todos App</h1>
-        <TodosList/>
+        <TodosList todos={this.state.todos}/>
       </div>
     );
   }
--- src/TodosList.js
+++ src/TodosList.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 export default class TodosList extends Component {
   render() {
+    console.log(this.props);
+
     return (
       <table>
         <thead>
