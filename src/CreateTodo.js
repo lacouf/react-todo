9dff6cbcdf2f24c4d0cdfa7d8fd15cfafd3108da
Francois Lacoursiere <lacouf@gmail.com>
2017-09-23 13:36:07 -0400

    form Submit with default behavior

--- src/CreateTodo.js
+++ src/CreateTodo.js
@@ -4,10 +4,14 @@ export default class CreateTodo extends Component {
 
   render() {
     return (
-      <form>
-        <input type="text" placeholder="what do I need to do" />
+      <form onSubmit={this.handleCreate.bind(this)}>
+        <input type="text" placeholder="what do I need to do" ref="createInput"/>
         <button>Create</button>
       </form>
     );
+  }
+
+  handleCreate() {
+    console.log(this.refs.createInput);
   }
 }
